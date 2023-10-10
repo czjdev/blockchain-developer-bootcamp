0a415440c8082c74da4b2a47fa8ae384be815fc6
chenzhijie <418525489@qq.com>
2023-10-10 10:50:14 +0800

    2.smoke test

---
+++ scripts/1_deploy.js
@@ -0,0 +1,17 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+    // fetch contract to deploy
+    const Token = await ethers.getContractFactory("Token");
+    // deploy contract
+    const token = await Token.deploy()
+    await token.deployed()
+    console.log(`Token deployed to:${token.address}`)
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
