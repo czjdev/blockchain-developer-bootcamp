ac0edd43dcaeac370f6f1bf8421c9e9498b2b14d
czj <418525489@qq.com>
2023-10-12 23:34:38 +0800

    5.token transfers

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,14 +9,27 @@ contract Token {
     uint256 public decimals = 18;
     uint256 public totalSupply;
 
-    // track balances
     mapping(address => uint256) public balanceOf;
-    // send tokens
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply * (10 ** decimals);
         balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        // deduct tokens from spender
+        balanceOf[msg.sender] -= _value;
+        // credit tokens to reciver
+        balanceOf[_to] += _value;
+        // emit event
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -6,16 +6,20 @@ const tokens = (n) => {
 };
 
 describe("Token", () => {
-  let token;
+  let token, accounts, deployer, reciver
 
   beforeEach(async () => {
     const Token = await ethers.getContractFactory("Token");
     token = await Token.deploy("Dapp University", "DAPP", "1000000");
+
+    accounts = await ethers.getSigners();
+    deployer = accounts[0];
+    reciver = accounts[1];
   });
 
   describe("Deployment", () => {
     const name = "Dapp University";
-    const symbol = "Dapp";
+    const symbol = "DAPP";
     const decimals = "18";
     const totalSupply = tokens("1000000");
 
@@ -34,5 +38,31 @@ describe("Token", () => {
     it("has correct total supply", async () => {
       expect(await token.totalSupply()).to.equal(totalSupply);
     });
+
+    it("assign total supply to deployer", async () => {
+      expect(await token.balanceOf(deployer.address)).to.equal(totalSupply);
+    });
   });
+
+  describe("Sending Token", () => {
+    let amount, transaction, result
+
+    beforeEach(async () => {
+      amount = tokens(100)
+      transaction = await token.connect(deployer).transfer(reciver.address, amount);
+      result = await transaction.wait()
+    })
+    it("Transfers token balances", async () => {
+      expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+      expect(await token.balanceOf(reciver.address)).to.equal(amount)
+    })
+    it("Emit a transfer event", async () => {
+      const event = result.events[0];
+      expect(event.event).to.equal("Transfer");
+      const args = event.args;
+      expect(args.from).to.equal(deployer.address);
+      expect(args.to).to.equal(reciver.address);
+      expect(args.value).to.equal(amount);
+    })
+  })
 });
