601b92eb16120c518be5c22b9c280e7458363a8a
czj <418525489@qq.com>
2023-10-17 07:24:22 +0800

    13.fill order

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -112,4 +112,13 @@ contract Exchange {
             block.timestamp
         );
     }
+
+    function fillOrder(uint256 _id) public {
+        _Order storage _order = orders[_id];
+        _trade(_order.id, _order.user, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive);
+    }
+
+    function _trade(uint256 _orderId, address _user, address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) internal {
+        
+    }
 }
