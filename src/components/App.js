2eedae13e16a42ef4c18695c6eb31c0e5d2b69f9
chenzhijie <418525489@qq.com>
2023-10-24 14:34:02 +0800

    17.redux pt 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,29 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import config from '../config.json';
-import { loadProvider, loadNetwork, loadAccount, loadToken } from '../store/interactions';
+import { loadProvider, loadNetwork, loadAccount, loadTokens, loadExchange } from '../store/interactions';
 
 function App() {
   const dispatch = useDispatch();
 
   const loadBlockchainData = async () => {
-    await loadAccount(dispatch);
-
-    // connnect to blockchain
+    // connect ethers to blockchain
     const provider = loadProvider(dispatch);
+
+    // fetch current network's chainId(e.g hardhat: 31337, kovan: 42)
     const chainId = await loadNetwork(provider, dispatch);
 
-    // token smart contract
-    await loadToken(provider, config[chainId].DApp.address, dispatch);
+    // fetch current account & balance from metamask
+    await loadAccount(provider, dispatch);
+
+    // load token smart contract
+    const DApp = config[chainId].DApp;
+    const mETH = config[chainId].mETH;
+    await loadTokens(provider, [DApp.address, mETH.address], dispatch);
+
+    // load exchange smart contract
+    const exchangeConfig = config[chainId].exchange;
+    await loadExchange(provider, exchangeConfig.address, dispatch);
   }
 
   useEffect(() => {
