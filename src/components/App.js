189ef4b58b9199097196b76dee9cb3559a896bb9
chenzhijie <418525489@qq.com>
2023-10-30 19:42:20 +0800

    23. maske orders

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import { loadProvider, loadNetwork, loadAccount, loadTokens, loadExchange, subsc
 import Navbar from './Navbar';
 import Markets from './Markets';
 import Balance from './Balance';
+import Order from './Order';
 
 function App() {
   const dispatch = useDispatch();
@@ -55,7 +56,7 @@ function App() {
 
           <Balance />
 
-          {/* Order */}
+          <Order />
 
         </section>
         <section className='exchange__section--right grid'>
