60db03dcd5a89354677336f02b579731eff45b59
chenzhijie <418525489@qq.com>
2023-11-01 20:31:50 +0800

    29.cancel orders

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,6 +1,7 @@
 import { useRef, useState } from 'react';
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { myOpenOrdersSelector, myFilledOrdersSelector } from '../store/selectors'
+import { cancelOrder } from '../store/interactions'
 
 import sort from '../assets/sort.svg';
 
@@ -8,10 +9,14 @@ import Banner from './Banner';
 
 const Transactions = () => {
   const [showMyOrders, setShowMyOrders] = useState(true)
+
+  const provider = useSelector(state => state.provider.connection)
   const symbols = useSelector(state => state.tokens.symbols)
   const myOpenOrders = useSelector(myOpenOrdersSelector)
   const myFilledOrders = useSelector(myFilledOrdersSelector)
+  const exchange = useSelector(state => state.exchange.contract)
 
+  const dispatch = useDispatch()
 
   const tradeRef = useRef(null)
   const orderRef = useRef(null)
@@ -26,6 +31,10 @@ const Transactions = () => {
       tradeRef.current.className = 'tab'
       setShowMyOrders(true)
     }
+  }
+
+  const cancelHandler = (order) => {
+    cancelOrder(provider, exchange, order, dispatch)
   }
 
   return (
@@ -59,7 +68,7 @@ const Transactions = () => {
                     <tr key={index}>
                       <td style={{ color: `${order.orderTypeClass}` }}>{order.token0Amount}</td>
                       <td>{order.tokenPrice}</td>
-                      <td>{/* TODO: Cancel order */}</td>
+                      <td><button className='button--sm' onClick={() => cancelHandler(order)}>Cancel</button></td>
                     </tr>
                   )
                 })}
--- src/store/interactions.js
+++ src/store/interactions.js
@@ -45,6 +45,11 @@ export const loadExchange = async (provider, address, dispatch) => {
 }
 
 export const subscribeToEvents = (exchange, dispatch) => {
+    exchange.on('Cancel', (id, user, tokenGet, amountGet, tokenGive, amountGive, timestamp, event) => {
+      const order = event.args
+      dispatch({ type: 'ORDER_CANCEL_SUCCESS', order, event })
+    })
+
     exchange.on('Deposit', (token, user, amount, balance, event) => {
       dispatch({ type: 'TRANSFER_SUCCESS', event })
     })
@@ -166,4 +171,21 @@ export const loadAllOrders = async (provider, exchange, dispatch) => {
   const allOrders = orderStream.map(event => event.args)
 
   dispatch({ type: 'ALL_ORDERS_LOADED', allOrders })
+}
+
+
+// ------------------------------------------------------------------------------
+// CANCEL ORDER
+
+export const cancelOrder = async (provider, exchange, order, dispatch) => {
+
+  dispatch({ type: 'ORDER_CANCEL_REQUEST' })
+
+  try {
+    const signer = await provider.getSigner()
+    const transaction = await exchange.connect(signer).cancelOrder(order.id)
+    await transaction.wait()
+  } catch (error) {
+    dispatch({ type: 'ORDER_CANCEL_FAIL' })
+  }
 }
